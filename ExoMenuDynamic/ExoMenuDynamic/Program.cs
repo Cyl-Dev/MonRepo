08c4620bff3816ee8580ff2ae004dbcfc712f4d2
Lapinte <nicolas.lapinte@gmail.com>
2018-05-31 16:25:42 +0200

    Liste d'objet

--- ExoMenuDynamic/ExoMenuDynamic/Program.cs
+++ ExoMenuDynamic/ExoMenuDynamic/Program.cs
@@ -13,23 +13,57 @@ namespace ExoMenuDynamic
         static void Main(string[] args)
         {
 
+            List<MenuItem> menuItems = new List<MenuItem>();
+            menuItems.Add(new MenuItem("Ajouter", 1));
+            menuItems.Add(new MenuItem("Supprimer", 2));
+            menuItems.Add(new MenuItem("Quitter", 5));
+ 
+            int menuItem = GererMenu(menuItems);
 
+            Console.WriteLine("Vous avez choisi : " + menuItem);
+
+            Console.ReadLine();
         }
 
-        private static int GererMenu(List<string> menu)
+        private static int GererMenu(List<MenuItem> menu)
         {
 
-            foreach (string ligne in menu)
+            foreach (MenuItem item in menu)
             {
-                Console.WriteLine(ligne);
+                Console.Write(item.Index + ". ");
+                Console.WriteLine(item.Libelle);
             }
 
-            int choix;
-            choix = int.Parse(Console.ReadLine());
-            return choix;
+            int numeroMenuValide = -1;
+            while (numeroMenuValide == -1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Entrer votre choix !");
+
+                string saisie = Console.ReadLine();
+                int numeroMenu = int.Parse(saisie);
+
+                foreach (MenuItem item in menu)
+                {             
+                    if (numeroMenu == item.Index) { numeroMenuValide = item.Index; break; }
+                }
+
+                if (numeroMenuValide == -1) Console.WriteLine("Choix Invalide !");
+            }
+
+            return numeroMenuValide;
         }
     }
 
+    public class MenuItem
+    {
+        public string Libelle { get; set; }
+        public int Index { get; set; }
 
-
+        public MenuItem(string libelle, int index)
+        {
+            Libelle = libelle;
+            Index = index;
+        }
+    }
 }
